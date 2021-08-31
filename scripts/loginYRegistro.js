a2958f8a42366ffc098d71fbd348a945ff3f9cbf
sabthe02 <sabthe02@web.de>
2021-08-30 23:46:03 -0300

    Adiciones login & registro
    Condiciones agregadas para logearse o regstrarse.

--- scripts/loginYRegistro.js
+++ scripts/loginYRegistro.js
@@ -21,14 +21,36 @@ function submitForm(event) {
     event.preventDefault()
 }
 
-function login() {
-    localStorage.setItem('logeado', true)
-    if (document.getElementById('email').value === "" || document.getElementById('password').value === "") {
-        alert('Por favor rellenar todos los campos') }
-    else {
-        console.log('algo anda mal')
-        window.location.href = 'home.html'  
+function validateEmail(email) 
+    {
+        let re = /\S+@\S+\.\S+/
+        return re.test(email)
+    }
+function registro() {
+    if (validateEmail(document.getElementById('emailRegistro').value) === true && document.getElementById('passwordRegistro').value === document.getElementById('passwordRegistroRepeat').value && localStorage.getItem('logeado') !=="true") {
+      localStorage.setItem('logeado', true)  
+      window.location.href = 'home.html'  
     }
     
-}
+    else if (localStorage.getItem('logeado') ==="true") {
+          alert('Usuario ya existente, por favor logearse')
+    }
+    
+    else {
+        alert('Las contraseñas deben ser iguales')
+    }
+}    
 
+function login() {
+      if (validateEmail(document.getElementById('email').value) === true && localStorage.getItem('logeado') ==="true") {
+        window.location.href = 'home.html' 
+      }
+
+      else if (localStorage.getItem('logeado') !=="true") {
+          alert('Usuario no existente, por favor registrarse')
+      }
+        
+      else {
+          alert('Ingresar correo electrónico válido')
+      }
+}
